5081f63ebedec99345b7e9176fe967e2c66c1e9d
Angeline <siv.angeline@ymail.com>
2019-10-15 18:42:58 -0400

    Added Project

---
+++ VehicleFactory/src/client/FactoryTester.java
@@ -0,0 +1,17 @@
+package client;
+import bus.*;
+
+public class FactoryTester
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        /*
+        To effectively manage the stock of vehicles, you must implement the following tasks:
+1.	Adding a new vehicle into a list of vehicles.
+2.	Searching for a vehicle by serial number.
+3.	Removing a specific vehicle from the list of vehicles.
+4.	Listing vehicles, listing gasoline vehicles, listing electric vehicles
+         */
+    }
+}
