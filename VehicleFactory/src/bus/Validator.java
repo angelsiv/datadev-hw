27970ba0b8b745777d1563cc4bfc155e9c7cf1e2
Angeline <siv.angeline@ymail.com>
2019-10-21 22:00:40 -0400

    Updated Validator class

--- VehicleFactory/src/bus/Validator.java
+++ VehicleFactory/src/bus/Validator.java
@@ -2,5 +2,14 @@ package bus;
 
 public class Validator
 {
-
+    try
+    {
+        Double.parseDouble();
+        System.out.println("This is a digit");
+    }
+		catch(Exception e)
+    {
+        System.out.println(e);
+        System.out.println("This is not a digit");
+    }
 }
--- VehicleFactory/src/client/FactoryTester.java
+++ VehicleFactory/src/client/FactoryTester.java
@@ -2,12 +2,14 @@ package client;
 import bus.*;
 import com.sun.deploy.security.SelectableSecurityManager;
 
+import javax.print.ServiceUI;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class FactoryTester
 {
-    ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
+
 
     public static void main(String[] args)
     {
@@ -18,9 +20,10 @@ public class FactoryTester
 3.	Removing a specific vehicle from the list of vehicles.
 4.	Listing vehicles, listing gasoline vehicles, listing electric vehicles
          */
-
+        ArrayList<Vehicle> vehicleList = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
         int menuChoice;
+
         //Main Menu
         do
         {
@@ -32,7 +35,10 @@ public class FactoryTester
 
         switch(menuChoice)
         {
-            case 1: System.out.println("Add Vehicle");
+            case 1: addVehicle(vehicleList);
+            case 2: removeVehicle(vehicleList);
+            case 3: displayVehicleList(vehicleList);
+            case 4: ExitApplication();
             default: break;
         }
     }
@@ -42,29 +48,30 @@ public class FactoryTester
         System.out.print("\t\n--MAIN MENU--");
     }
 
-    public static void addVehicle(Vehicle vehicle, ArrayList<Vehicle> list)
+    public static void addVehicle(ArrayList<Vehicle> list)
     {
         list.add(vehicle);
         System.out.print("Successfully added a new Vehicle");
     }
 
-    public static void removeVehicle(Vehicle vehicle)
+    public static void removeVehicle(ArrayList<Vehicle> list)
     {
         System.out.println("\t\n--REMOVE VEHICLE--");
     }
 
     public static void displayVehicleList(ArrayList<Vehicle> list)
     {
-        updateVehicleSorting();
+        updateVehicleSorting(list);
         for (Vehicle element: list)
         {
             System.out.println(element);
         }
     }
 
-    public static void updateVehicleSorting()
+    public static void updateVehicleSorting(ArrayList<Vehicle> list)
     {
-
+        SerialNumberComparator serialNumberComparator = new SerialNumberComparator();
+        list.sort(serialNumberComparator);
     }
 
     public static void ExitApplication()
